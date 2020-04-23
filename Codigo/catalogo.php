3400eedf7cc694e35cae7526164fc48b9c0f1b19
Mauricio Alvarez <maualvm@gmail.com>
2020-04-23 18:55:50 -0500

    Arreglar espacio en filtro

--- Codigo/catalogo.php
+++ Codigo/catalogo.php
@@ -5,11 +5,13 @@
 
 
 <div id="modal-editar" class="uk-modal-container" uk-modal></div>
-<div class="uk-container uk-width-5-6@l uk-width-1-1@s">
-    <h2>Nuestros Perros
-    <a href="agregarPerro.php" class="uk-icon-link uk-align-right" uk-icon="plus-circle"; ratio = "2"></a>
-    </h2>
-    <div id="filterMenu" class="uk-height-max-large uk-align-left uk-width-1-4@l uk-width-1-1@s">
+<div class="uk-container uk-margin-top">
+    <h1>Nuestros Perros
+        <a href="agregarPerro.php" class="uk-icon-link uk-align-right" uk-icon="plus-circle"; ratio = "2"></a>
+    </h1>
+</div>
+<div id="main" class="uk-flex">
+    <div id="filterMenu" class="uk-container uk-flex-left">
         <ul id="listaFiltro" class="uk-nav-primary uk-nav-parent-icon" uk-nav="multiple: true">
             <li class="uk-parent">
                 <a href="#">Filtros</a>
@@ -20,7 +22,6 @@
                     <hr>
                     <li>Edad</li>
                     <li>
-
                         <div id="ageSlider"></div> <div id="ageSlider-value"></div>
                         <div class="hidden" hidden>
                             <input id="minAge" name="minAge" type="number" class="validate">
@@ -48,33 +49,31 @@
                     <li>
                         <label>
                             <input class="uk-radio" name="order" type="radio" value="asc"/>
-                            <span>Ascendiente</span>
+                            <span>Ascendente</span>
                         </label>
                     </li>
                     <li>
                         <label>
                             <input class="uk-radio" name="order" type="radio" value="desc"/>
-                            <span>Descendiente</span>
+                            <span>Descendente</span>
                         </label>
                     </li>
                     <hr>
                 </ul>
             </li>
+            <button id="filtrar" class="uk-button uk-button-primary uk-align-right">Aplicar</button>
         </ul>
-
-        <button id="filtrar" class="uk-button uk-button-primary uk-align-right">Aplicar</button>
     </div>
-
-    <div class="uk-child-width-1-3@m" id="contenido-catalogo" uk-grid>
-
+        <hr class="uk-divider-vertical uk-height-large">
 
 
 
-    <?php
-
-        include("controlador_catalogo.php");
-
-    ?>
+    <div class="uk-container uk-flex-right">
+        <div class="uk-child-width-1-3@m" id="contenido-catalogo" uk-grid>
+        <?php
+            include("controlador_catalogo.php");
+        ?>
+        </div>
     </div>
 </div>
 
