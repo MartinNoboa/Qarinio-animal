8bead562f9a0f0d48376ce786f06b467306c1aaf
MartinNoboa <nomonoboa@gmail.com>
2020-04-23 19:45:33 -0500

    Terminar interzas y agregar funciones base para requisito

--- Codigo/agregarPerro.php
+++ Codigo/agregarPerro.php
@@ -1,6 +1,7 @@
 <?php 
-    include("_header.html");
-    include("_navbar.html");
+require_once "util.php";
+include("_header.html");
+include("_navbar.html");
 ?>
 
 
@@ -13,12 +14,12 @@
 
         <div class="uk-margin">
             <h5>Nombre</h5>
-            <input class="uk-input" type="text" placeholder="nombre">
+            <input class="uk-input" type="text" placeholder="Nombre" id = "nombre">
         </div>
 
         <div class="uk-margin">
             <h5>Tamaño</h5>
-            <select class="uk-select">
+            <select class="uk-select" id = "size">
                 <option>Tamaño...</option>
                 <option>Pequeño</option>
                 <option>Mediano</option>
@@ -27,17 +28,17 @@
         </div>
         <h5>Edad</h5>
         <div class="uk-width-1-4@s">
-                <input class="uk-input" type="number" placeholder="Años">
+                <input class="uk-input" type="number" placeholder="Años" id = "years">
         </div>
         <div class = "uk-margin-small-top">    
             <div class="uk-width-1-4@s">
-                <input class="uk-input" type="number" placeholder="Meses">
+                <input class="uk-input" type="number" placeholder="Meses" id = "meses">
             </div>
         </div>
         
         <div class = "uk-margin">
             <h5>Fecha de Llegada</h5>
-            <input class = "uk-input" type = "date">
+            <input class = "uk-input" type = "date" id = "fecha">
         </div>
         
             <h5>Género</h5>
@@ -45,8 +46,25 @@
             <label><input class="uk-radio" type="radio" name="radio2" checked> Macho</label>
             <label><input class="uk-radio" type="radio" name="radio2"> Hembra</label>
         </div>
-        
-         <div class="uk-margin">
+        <div class="uk-margin">
+            <h5>Condiciones Medicas</h5>
+            <select class="uk-select" id = "condiciones" name = "condiciones">
+                <option></option>
+            </select>
+        </div>
+        <div class="uk-margin">
+            <h5>Personalidad</h5>
+            <select class="uk-select" id = "personalidad" name = "personalidad">
+                <option>Recuperar de la base de datos</option>
+            </select>
+        </div>
+        <div class="uk-margin">
+            <h5>Raza</h5>
+            <select class="uk-select" id = "raza" name = "raza">
+                <option>Recuperar de la base de datos</option>
+            </select>
+        </div>
+        <div class="uk-margin">
             <h5>Historia del perro</h5>
             <textarea class="uk-textarea" rows="7" placeholder="Historia"></textarea>
         </div>
