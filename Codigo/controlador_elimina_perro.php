6f88ed380ab0e191acc4fa5440a676229bfbffa2
Jose Eduardo Cadena Bernal <A01704641@itesm.mx>
2020-04-27 16:31:31 -0500

    Agregar eliminar perro, no terminado

---
+++ Codigo/controlador_elimina_perro.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require_once "util.php";
+
+$perro = isset($_POST["idPerro"]);
+echo "el perro se elimino";
+
+//$result = eliminar_perro($idPerro);
+/*
+if( $result ) {
+    echo "el perro se elimino";
+  }else{
+    echo "el perro no  se elimino";
+  }
+  
+
+*/
+
+?>
