2d21ba988c6a5d6e70b5639b7c65936eb649336c
MartinNoboa <nomonoboa@gmail.com>
2020-04-26 21:46:17 -0500

    Editar form agregar perro

--- src/catalogo.php
+++ src/catalogo.php
@@ -5,9 +5,9 @@
 
 
 <div id="modal-editar" class="uk-modal-container" uk-modal></div>
-<div class="uk-container uk-margin-top">
+<div class="uk-container uk-margin-large-top">
     <h1>Nuestros Perros
-        <a href="agregarPerro.php" class="uk-icon-link uk-align-right" uk-icon="plus-circle"; ratio = "2"></a>
+        <a href="agregarPerro.php" class="uk-icon-link uk-align-right" uk-icon="plus-circle"; ratio = "2"; uk-tooltip = "Agregar perro"></a>
     </h1>
 </div>
 <div id="main" class="uk-flex">
