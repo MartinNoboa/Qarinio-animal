d4fb30976ee38684055ec19b324667e706a29546
MartinNoboa <nomonoboa@gmail.com>
2020-04-29 08:15:45 -0500

    Revertir errores

--- src/agregarPerro.php
+++ src/agregarPerro.php
@@ -1,4 +1,5 @@
 <?php 
+
     include("_header.html");
     include("_navbar.html");
     include_once("util.php");
@@ -7,11 +8,6 @@
      foreach($_POST as &$key){
         $key = limpia_entrada($key);
     }
-
-    if(!isset($_SESSION["error"])) {
-            $_SESSION["error"] = false;
-        }
-
 
     $camposRequeridos = [
         "nombre",
@@ -37,21 +33,24 @@
         $raza = $_POST["raza"];
         $historia = $_POST["historia"];
         
-        agregarPerro($nombre,$size,$meses, $fechaLlegada, $genero, $historia, $idCondicion,$idRaza, $idPersonalidad);
-        
-         /*if(!verificaCampos($_POST,$camposRequeridos)){
+         if(!verificaCampos($_POST,$camposRequeridos)){
              $_SESSION["error"] = "Debes llenar todos los campos";
     }else{
         agregarPerro($nombre,$size,$meses, $fechaLlegada, $genero, $historia, $idCondicion,$idRaza, $idPersonalidad);
-        }*/
+        }
     
         
     }
+   
+    
+    
+        
+
+    
 ?>
 
-
    <div class = "uk-container">
-    <form>
+    <form action = "agregarPerro.php" method = "POST">
         
         <fieldset class="uk-fieldset">
 
@@ -59,47 +58,69 @@
 
         <div class="uk-margin">
             <h5>Nombre</h5>
-            <input class="uk-input" type="text" placeholder="nombre">
+            <input class="uk-input" type="text" placeholder="Nombre" name = "nombre" id = "nombre">
         </div>
 
         <div class="uk-margin">
             <h5>Tamaño</h5>
-            <select class="uk-select">
-                <option>Tamaño...</option>
-                <option>Pequeño</option>
-                <option>Mediano</option>
-                <option>Grande</option>
+            <select class="uk-select" name = "size">
+                <option selected hidden>Tamaño...</option>
+                <option value = "Pequenio">Pequeño</option>
+                <option value = "mediano">Mediano</option>
+                <option value = "grande">Grande</option>
             </select>
         </div>
         <h5>Edad</h5>
-        <div class="uk-width-1-4@s">
-                <input class="uk-input" type="number" placeholder="Años">
-        </div>
         <div class = "uk-margin-small-top">    
             <div class="uk-width-1-4@s">
-                <input class="uk-input" type="number" placeholder="Meses">
+                <input class="uk-input" type="number" placeholder="Meses" id = "meses" name= "meses">
             </div>
         </div>
         
         <div class = "uk-margin">
             <h5>Fecha de Llegada</h5>
-            <input class = "uk-input" type = "date">
+            <input class = "uk-input" type = "date" id = "fecha" name = "fecha">
         </div>
         
             <h5>Género</h5>
         <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
-            <label><input class="uk-radio" type="radio" name="radio2" checked> Macho</label>
-            <label><input class="uk-radio" type="radio" name="radio2"> Hembra</label>
+            <label><input class="uk-radio" type="radio" name="genero"  value = "macho" checked> Macho</label>
+            <label><input class="uk-radio" type="radio" name="genero" value = "hembra"> Hembra</label>
+        </div>
+        <div class="uk-margin">
+            <h5>Condiciones Medicas</h5>
+            <select class="uk-select" id = "condiciones" name = "condiciones">
+                <option selected hidden value = "">Seleccione una opcion...</option>
+                <?= recuperarOpciones(idCondicion, condicion, condiciones_medicas) ?>
+            </select>
+        </div>
+        <div class="uk-margin">
+            <h5>Personalidad</h5>
+            <select class="uk-select" id = "personalidad" name = "personalidad">
+                <option selected hidden value = "">Seleccione una opcion...</option>
+                <?= recuperarOpciones(idPersonalidad, personalidad, tipo_personalidad) ?>
+            </select>
+        </div>
+        <div class="uk-margin">
+            <h5>Raza</h5>
+            <select class="uk-select" id = "raza" name = "raza">
+                <option selected hidden value = "">Seleccione una opcion...</option>
+                <?= recuperarOpciones(idRaza, raza, tipo_raza) ?>
+            </select>
+            </div>
+        <div class="uk-margin">
+            <h5>Historia del perro</h5>
+            <textarea class="uk-textarea" rows="7" placeholder="Historia" name = "historia"></textarea>
+        </div>
+        <div class="uk-margin">
+            <button type = "submit"  id = "agregar" class = "uk-button uk-button-primary uk-position-relative uk-position-center uk-margin-large-top">Agregar perro</button>
         </div>
         
-         <div class="uk-margin">
-            <h5>Historia del perro</h5>
-            <textarea class="uk-textarea" rows="7" placeholder="Historia"></textarea>
-        </div>
         </fieldset>
-        <button class = "uk-button uk-button-primary uk-position relative uk-position-center" type = "submit" id = "submit">Agregar perro</button>
     </form>
+    
 </div>
+
 
 <?php else:
     http_response_code(404);
