e0cbeba4f456e74d5adeff70a665924296dee483
Mauricio Alvarez <maualvm@gmail.com>
2020-05-15 15:07:26 -0500

    Crear interfaz inicial

---
+++ src/misSolicitudes.php
@@ -0,0 +1,55 @@
+<?php
+    include '_header.html';
+    include '_navbar.html';
+?>
+<div class="uk-container">
+    <h1 class="uk-text-center uk-margin-top">Mis Solicitudes de Adopción</h1>
+    <table class="uk-table uk-table-striped">
+        <thead clas>
+            <tr>
+                <th>Perro</th>
+                <th class="uk-text-center">Formulario</th>
+                <th class="uk-text-center">Entrevista</th>
+                <th class="uk-text-center">Pago</th>
+                <th class="uk-text-center">Editar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1 (Hard Code)</td>
+                <td class="uk-text-center uk-alert-success"><span uk-icon="icon: check"></span></td>
+                <td class="uk-text-center uk-alert-danger"><span uk-icon="icon: close"></span></td>
+                <td class="uk-text-center uk-alert-warning"><span uk-icon="icon: minus"></span></td>
+                <td class="uk-text-center"><a class="uk-link-text" href="#"><span uk-icon="icon: file-edit"></span></a></td>
+            </tr>
+            <tr>
+                <td>2 (Hard Code)</td>
+                <td class="uk-text-center uk-alert-success"><span uk-icon="icon: check"></span></td>
+                <td class="uk-text-center uk-alert-danger"><span uk-icon="icon: close"></span></td>
+                <td class="uk-text-center uk-alert-warning"><span uk-icon="icon: minus"></span></td>
+                <td class="uk-text-center"><a class="uk-link-text" href="#"><span uk-icon="icon: file-edit"></span></a></td>
+            </tr>
+            <tr>
+                <td>3 (Hard Code)</td>
+                <td class="uk-text-center uk-alert-success"><span uk-icon="icon: check"></span></td>
+                <td class="uk-text-center uk-alert-danger"><span uk-icon="icon: close"></span></td>
+                <td class="uk-text-center uk-alert-warning"><span uk-icon="icon: minus"></span></td>
+                <td class="uk-text-center"><a class="uk-link-text" href="#"><span uk-icon="icon: file-edit"></span></a></td>
+            </tr>
+            <tr>
+                <td>4 (Hard Code)</td>
+                <td class="uk-text-center uk-alert-success"><span uk-icon="icon: check"></span></td>
+                <td class="uk-text-center uk-alert-danger"><span uk-icon="icon: close"></span></td>
+                <td class="uk-text-center uk-alert-warning"><span uk-icon="icon: minus"></span></td>
+                <td class="uk-text-center"><a class="uk-link-text" href="#"><span uk-icon="icon: file-edit"></span></a></td>
+            </tr>
+            <tr>
+                <td>5 (Hard Code)</td>
+                <td class="uk-text-center uk-alert-success"><span uk-icon="icon: check"></span></td>
+                <td class="uk-text-center uk-alert-danger"><span uk-icon="icon: close"></span></td>
+                <td class="uk-text-center uk-alert-warning"><span uk-icon="icon: minus"></span></td>
+                <td class="uk-text-center"><a class="uk-link-text" href="#"><span uk-icon="icon: file-edit"></span></a></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
