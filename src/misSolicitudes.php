7f3bc7362f11c2b52e5e740f262805ecfa97fdf6
Mauricio Alvarez <maualvm@gmail.com>
2020-05-16 16:14:26 -0500

    Eliminar tabla comentada

--- src/misSolicitudes.php
+++ src/misSolicitudes.php
@@ -10,40 +10,6 @@
     <form class="uk-form uk-align-right" action="catalogo.php" method="post">
         <button class="uk-button uk-button-primary uk-border-rounded" type="submit" name="button">Adoptar un perro</button>
     </form>
-    <!-- en lugar de meter la tabla, hacer función PHP para mostrar los formularios -->
-    <!-- <table class="uk-table uk-table-divider uk-table-large uk-table-hover uk-animation-slide-bottom-medium">
-        <thead clas>
-            <tr>
-                <th class="uk-text-center uk-width-small uk-text-secondary">Perro</th>
-                <th class="uk-text-center uk-text-secondary">Formulario</th>
-                <th class="uk-text-center uk-text-secondary">Entrevista</th>
-                <th class="uk-text-center uk-text-secondary">Pago</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr onclick="window.location='catalogo.php';">
-                <td>1 (Hard Code)</td>
-                <td class="uk-text-center"><a class="uk-link-text" href="#"><span class="uk-text-center uk-text-success" uk-icon="icon: check" uk-tooltip="title: ¡Tu formulario fue aprobado!"></a></span></td>
-                <td class="uk-text-center"><a class="uk-link-text" href="https://www.youtube.com"><span class="uk-text-center uk-text-danger" uk-icon="icon: close" uk-tooltip="title: Tu entrevista fue rechazada"></a></span></td>
-                <td class="uk-text-center"><a class="uk-link-text" href="https://www.gmail.com"><span class="uk-text-center uk-text-warning" uk-icon="icon: minus" uk-tooltip="title: Tu pago está pendiente"></a></span></td>
-                <td class="uk-text-center"><a class="uk-link-text" href="#"><span uk-icon="icon: file-edit"></span></a></td>
-            </tr>
-            <tr onclick="window.location='index.php';">
-                <td>2 (Hard Code)</td>
-                <td class="uk-text-center"><span class="uk-text-center uk-text-danger" uk-icon="icon: close" uk-tooltip="title: Tu formulario fue rechazado"></span></td>
-                <td class="uk-text-center"><span class="uk-text-center uk-text-success" uk-icon="icon: check" uk-tooltip="title: ¡Tu entrevista fue aprobada!"></span></td>
-                <td class="uk-text-center"><span class="uk-text-center uk-text-warning" uk-icon="icon: minus"></span></td>
-                <td class="uk-text-center"><a class="uk-link-text" href="#"><span uk-icon="icon: file-edit"></span></a></td>
-            </tr>
-            <tr>
-                <td>3 (Hard Code)</td>
-                <td class="uk-text-center"><span class="uk-text-center uk-text-success" uk-icon="icon: check"></span></td>
-                <td class="uk-text-center"><span class="uk-text-center uk-text-danger" uk-icon="icon: close"></span></td>
-                <td class="uk-text-center"><span class="uk-text-center uk-text-warning" uk-icon="icon: minus"></span></td>
-                <td class="uk-text-center"><a class="uk-link-text" href="#"><span uk-icon="icon: file-edit"></span></a></td>
-            </tr>
-        </tbody>
-    </table> -->
     <?= muestraSolicitudes(); ?>
 </div>
 <?php
