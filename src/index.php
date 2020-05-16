7a1341f6216b44173a8566233f9f64c06453091d
Mauricio Alvarez <maualvm@gmail.com>
2020-05-16 12:48:38 -0500

    Agregar texto a index

--- src/index.php
+++ src/index.php
@@ -3,11 +3,12 @@
   include("_header.html");
   include("_navbar.html");
 ?>
-
-<div class="uk-height-large uk-background-cover uk-light uk-flex" uk-parallax="bgy: -200" style="background-image: url('img/Mario.jpg');" uk-height-viewport="min-height: 10">
-    <h1 class="uk-text-center uk-margin-auto uk-margin-auto-vertical">Qariño Animal</h1>
-</div>
-
+    <div class="uk-height-large uk-background-cover uk-overflow-hidden uk-light uk-flex uk-flex-top" style="background-image: url('img/Mario.jpg');" uk-parallax="bgy: -150">
+        <div class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">
+            <h1 class="uk-text-bold">Qariño Animal</h1>
+            <h3 class="uk-text-bold">Espacio creado para la expresión de animalistas, ayuda y difusión de adopciones</h3>
+        </div>
+    </div>
 
 <script>
 UIkit.parallax(element, options);
