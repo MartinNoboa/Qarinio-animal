531efaf00324bcb05bb6b8b98f150f005220cda2
Mauricio Alvarez <maualvm@gmail.com>
2020-05-16 22:42:42 -0500

    Agregar boton pantalla de inicio

--- src/index.php
+++ src/index.php
@@ -7,6 +7,9 @@
         <div class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">
             <h1 class="uk-text-bold">Qariño Animal</h1>
             <h3 class="uk-text-bold">Espacio creado para la expresión de animalistas, ayuda y difusión de adopciones</h3>
+            <form class="uk-form" action="catalogo.php" method="post">
+                <button class="uk-button uk-button-primary uk-border-pill" type="submit" name="button">¡Adoptar un perro ahora!</button>
+            </form>
         </div>
     </div>
 
