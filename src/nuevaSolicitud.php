8696212ca956fdeb0f614f28a8c20b40ecc817f6
Mauricio Alvarez <maualvm@gmail.com>
2020-05-18 19:34:02 -0500

    Agregar boton de retroceso

--- src/nuevaSolicitud.php
+++ src/nuevaSolicitud.php
@@ -8,11 +8,14 @@ if(checkPriv("adoptar")):
     <h1 class="uk-text-center">Nueva Solicitud de Adopción</h1>
     <hr class="uk-divider-icon">
     <h2 class="uk-text-center">Formulario de Adopción</h2>
+    <a href='catalogo.php' uk-tooltip = 'Click para retroceder' class='uk-icon-link uk-align-left' uk-icon='arrow-left'; ratio ='2'></a>
     <div class="uk-margin-xlarge-right uk-margin-xlarge-left">
-        <form class="uk-form" action="index.html" method="post">
+        <form class="uk-form" action="vista_agendar_entrevista.php" method="post">
             <?= muestraPreguntasFormulario(); ?>
             <hr>
-            <button type="submit" name="button" class="uk-button uk-button-primary uk-border-rounded">Enviar</button>
+            <div class="uk-align-right">
+                <button type="submit" name="button" class="uk-button uk-button-primary uk-border-rounded ">Enviar</button>
+            </div>
         </form>
     </div>
 </div>
