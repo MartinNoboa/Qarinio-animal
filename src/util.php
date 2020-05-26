4fec30e669d76cc39442e094913f388d74c179ab
MartinNoboa <nomonoboa@gmail.com>
2020-05-26 18:19:59 -0500

    Corregir errorer segundo query

--- src/util.php
+++ src/util.php
@@ -496,7 +496,6 @@ function nuevaSolicitud ($idUsuario, $idPerro,$res1, $res2,$res3,$res4,$res5,$re
     $sql2 = "CALL crearFormulario((SELECT idSolicitud FROM solicitud ORDER BY idSolicitud DESC LIMIT 1),$res1, $res2,$res3,$res4,$res5,$res6,$res7,$res8,$res9,$res10,$res11,$res12)";
     
     $result1 = sqlqry($sql1); 
-    print_r($result1);
     
     if($result1){
         $result2 = sqlqry($sql2); 
