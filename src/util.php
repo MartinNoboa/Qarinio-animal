34d06f77dfb5bee544cadbec401f0f06ff40bd8d
MartinNoboa <nomonoboa@gmail.com>
2020-05-29 14:31:46 -0500

    martin-mau/admin-actualiza-pago

--- src/util.php
+++ src/util.php
@@ -414,6 +414,7 @@ WHERE u.idUsuario=s.idUsuario AND p.idPerro=s.idPerro AND u.idUsuario='".$_SESSI
         elseif($row['Formulario'] == 3) { //incompleto
             $tabla .= "<td class=\"uk-text-center\"><a class=\"uk-link-text\" href=\"#\"><span class=\"uk-text-center uk-text-danger\" uk-icon=\"icon: close\" uk-tooltip=\"title: Tu formulario fue rechazado\"></a></span></td>";
         }
+        
 
         if($row['Entrevista'] == 5) { //completado
             $tabla .= "<td class=\"uk-text-center\"><a class=\"uk-link-text\" href=\"#\"><span class=\"uk-text-center uk-text-success\" uk-icon=\"icon: check\" uk-tooltip=\"title: ¡Tu entrevista fue aprobada!\"></a></span></td>";
