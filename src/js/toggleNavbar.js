7ba1543a9d02d9a7ee874691784f653702e23dfb
Mauricio Alvarez <maualvm@gmail.com>
2020-06-04 16:01:26 -0500

    Agregar off canvas con hamburger

--- src/js/toggleNavbar.js
+++ src/js/toggleNavbar.js
@@ -1,24 +1,8 @@
-//media query event handler
-if(matchMedia){
-    const mq = window.matchMedia( "(max-width:768px)" );//sets the width you want
-    mq.addListener(WidthChange);
-    WidthChange(mq)
+function toggleOffCanvas() {
+    UIkit.offcanvas("#my-id").toggle();
+    document.getElementById('hamburger-btn').classList.add("is-active");
 }
 
-function WidthChange(mq){
-    if(mq.matches){
-        //if window width is less than or equal to 768px;
-        $("#navbar_toggle").removeClass("dis-none"); //makes toggler visible
-        $("#navbar_collapse").addClass("dis-none");//makes the menu not visible initially
-        //so you can only see the menu items when you click the toggler
-
-        $("#navbar_toggle").on('click', function(e){
-            $("#navbar_collapse").slideToggle();
-            $("#navbar_collapse").css({"display": "block"});
-        })
-    }else{
-        //for larger screens
-        $("#navbar_toggle").addClass('dis-none');
-        $("#navbar_collapse").removeClass('dis-none');
-    }
-}
+UIkit.util.on("#my-id", "hide", function(){
+    document.getElementById("hamburger-btn").classList.remove("is-active");
+});
