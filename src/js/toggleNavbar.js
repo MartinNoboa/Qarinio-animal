21daaadf07ac93a190121b0829c0f67a13c96f9b
BernardoEstrada <estradaf.bernardo@gmail.com>
2020-06-04 16:45:06 -0500

    Finaliza navbar movil

--- src/js/toggleNavbar.js
+++ src/js/toggleNavbar.js
@@ -1,8 +1,24 @@
-function toggleOffCanvas() {
-    UIkit.offcanvas("#my-id").toggle();
-    document.getElementById('hamburger-btn').classList.add("is-active");
-}
+let open = false;
 
-UIkit.util.on("#my-id", "hide", function(){
-    document.getElementById("hamburger-btn").classList.remove("is-active");
-});
+function toggleOffCanvas() {
+    let offC = UIkit.offcanvas("#my-id");
+    if(open){
+        offC.hide();
+    } else{
+        offC.show();
+    }
+}
+UIkit.util.on("#my-id", "hide", ()=>document.getElementById("hamburger-btn").classList.remove("is-active"));
+UIkit.util.on("#my-id", "hidden", ()=>open=false);
+
+UIkit.util.on("#my-id", "show", ()=>document.getElementById("hamburger-btn").classList.add("is-active"));
+UIkit.util.on("#my-id", "shown", ()=>open=true);
+
+// UIkit.util.on("#my-id", "hidden", function(){
+//     document.getElementById("hamburger-btn").classList.remove("is-active");
+//     open = false;
+// });
+// UIkit.util.on("#my-id", "shown", function(){
+//     document.getElementById("hamburger-btn").classList.add("is-active");
+//     open = true;
+// });
