dd732df4d3a9181626a13aade6ecdb71c6a6326b
Mauricio Alvarez <maualvm@gmail.com>
2020-06-08 18:43:59 -0500

    Corregir crear cuenta

--- src/crearCuenta.php
+++ src/crearCuenta.php
@@ -1,7 +1,6 @@
 <?php
     include_once ("util.php");
     include("_header.html");
-    include("_navbar.html");
     foreach($_POST as &$key){
         $key = limpia_entrada($key);
     }
@@ -13,6 +12,9 @@
         $_SESSION["error"] = false;
     }
 
+    if (!($_SESSION["createState"] == 2 && $_POST["submit"] == "Terminar")) {
+        include("_navbar.html");
+    }
     $camposRequeridos = [
         "nombre",
         "apellido",
@@ -68,7 +70,7 @@
                 "registrado-no-verificado"
             ];
             if(crearCuenta(...$datosCuenta)){
-                $_SESSION["mensaje"]="Se ha creado la sesión con éxito";
+                $_SESSION["mensaje"]="Se ha creado la cuenta con éxito.\nTe hemos enviado un correo de confirmación.";
                 $_SESSION["createState"]=null;
                 autenticar($_POST["email"], $_POST["contrasenia"]);
                 header("location:/");
