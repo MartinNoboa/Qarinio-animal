5c945c245d9244ae1c9abea0be278a0087370a7d
BernardoEstrada <estradaf.bernardo@gmail.com>
2020-06-11 18:10:19 -0500

    Arregla cancelar solicitud OTRA VEZ

--- src/controlador_aprobar_solicitud.php
+++ src/controlador_aprobar_solicitud.php
@@ -8,7 +8,7 @@ session_start();
 if(checkPriv("ver-todas-solicitudes")) {
         echo aprobarSolicitud($id);
 } else {
-    echo "Hubo un error";
+    echo 0;
 }
 
 ?>
--- src/controlador_rechazar_solicitud.php
+++ src/controlador_rechazar_solicitud.php
@@ -4,12 +4,11 @@ include_once("util.php");
 session_start();
 
 if(checkPriv("ver-todas-solicitudes")) {
-    include("util.php");
     $id = limpia_entrada($_POST["idSolicitud"]);
 
     echo rechazarSolicitud($id);
 } else {
-    echo "Hubo un error";
+    echo 0;
 }
 
 ?>
